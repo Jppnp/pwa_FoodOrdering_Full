4ec039c789dbdc60efb43bd8bc29c634ec740257
Jppnp <pkarjiewpa@gmail.com>
2023-03-18 16:49:37 +0700

    New Navbar

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import { Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div>
-      <Nav variant="pills" defaultActiveKey={"/"}>
+      {/* <Nav variant="pills" defaultActiveKey={"/"}>
         <Nav.Item style={{width: "50%"}}>
           <Nav.Link as={Link} to="/">
             Home
@@ -20,7 +20,18 @@ function Header() {
             Add User
           </Nav.Link>
         </Nav.Item>
-      </Nav>
+      </Nav> */}
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to ="/">Home</Nav.Link>
+            <Nav.Link as={Link} to ="/users">Show Users</Nav.Link>
+            <Nav.Link as={Link} to ="/add">Add User</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
     </div>
   );
 }
