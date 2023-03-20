68b76143875818255e9e6228133fe77ca2e65a34
Jppnp <pkarjiewpa@gmail.com>
2023-03-20 12:11:59 +0700

    offline fetch

--- Frontend/src/components/ShowUsers.jsx
+++ Frontend/src/components/ShowUsers.jsx
@@ -1,34 +1,69 @@
 import { Button, Card, Col, Row } from "react-bootstrap";
-import axios from "axios";
+// import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 import { TiDelete, TiSpanner } from "react-icons/ti";
 import { Link } from "react-router-dom";
 
-const client = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-});
+// const client = axios.create({
+//   baseURL: process.env.REACT_APP_BASE_URL,
+// });
 
 function ShowUsers() {
   const [datas, setData] = useState([]);
+  const [mode, setMode] = useState("online");
+
+  // useEffect(() => {
+  //   client
+  //     .get("users")
+  //     .then((respone) => {
+  //       console.log(respone);
+  //       setData(respone.data);
+  //       localStorage.setItem("users", JSON.stringify(respone));
+  //     })
+  //     .catch((err) => {
+  //       setMode('offline');
+  //       let collection = localStorage.getItem("users");
+  //       console.log("Already get item in localStorage")
+  //       setData(JSON.parse(collection));
+  //     });
+  // }, []);
 
   useEffect(() => {
-    client.get("users").then((respone) => {
-      console.log(respone);
-      setData(respone.data);
-    });
+    let url = process.env.REACT_APP_BASE_URL;
+    fetch(url + `/users`)
+      .then((response) => {
+        response.json().then((result) => {
+          console.warn(result);
+          setData(result);
+          localStorage.setItem("users", JSON.stringify(result));
+        });
+      })
+      .catch((err) => {
+        setMode("offline");
+        let collection = localStorage.getItem("users");
+        setData(JSON.parse(collection));
+      });
   }, []);
 
-  function DeleteUser(event) {
-    var fid = event.target.id;
-    try {
-      client.delete("/" + fid).then(window.location.reload());
-    } catch (err) {
-      console.error(`Delete`, err);
-    }
-  }
+  // function DeleteUser(event) {
+  //   var fid = event.target.id;
+  //   try {
+  //     client.delete("/" + fid).then(window.location.reload());
+  //   } catch (err) {
+  //     console.error(`Delete`, err);
+  //   }
+  // }
   return (
     <div>
+      <div>
+        {mode === "offline" ? (
+          <div class="alert alert-warning" role="alert">
+            {" "}
+            you are in offline mode
+          </div>
+        ) : null}
+      </div>
       <h2>All Users !!! </h2>
       <Row xs={1} md={2} className="g-4" style={{ margin: "1rem" }}>
         {datas.map((data) => {
@@ -41,18 +76,18 @@ function ShowUsers() {
                     <p>Name: {data.name}</p>
                     <p>Email: {data.email}</p>
                     <p>phone: {data.phone}</p>
-                    <Link to={`/update`} state={{ id: data.id}} >
+                    <Link to={`/update`} state={{ id: data.id }}>
                       <Button className="btn btn-warning">
                         <TiSpanner />
                       </Button>
                     </Link>
-                    <Button
+                    {/* <Button
                       id={data.id}
                       onClick={DeleteUser}
                       className="btn btn-danger"
                     >
                       <TiDelete />
-                    </Button>
+                    </Button> */}
                   </Card.Body>
                 </Card>
               </Col>
